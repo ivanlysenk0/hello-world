ca4ee50f1c03d4a92af80390fae9263d1f868d72
ivanlysenk0 <72573715+ivanlysenk0@users.noreply.github.com>
2021-03-07 02:00:44 +0200

    Add files via upload

---
+++ hw_52_variable_n.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import static java.lang.System.out;
+
+public class Main<m, b, c, n, d, a> {
+
+    public static void main(String[] args) {
+        int n;
+        n = 9;
+        if (n % 2 == 0) out.println(" n " + "четное число");
+        else out.println(" n " + "нечетное число");
+    }
+}
